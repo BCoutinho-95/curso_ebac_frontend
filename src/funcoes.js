f5ab95597127f5faef2a2956b9751c1dca11f614
BCoutinho-95 <bcoutinhoti@gmail.com>
2024-07-23 15:27:04 -0300

    Add files via upload

---
+++ src/funcoes.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(a, b) {
+    return a * b;
+}
+console.log(multiplica(5, 10));
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(saudacao('Bruno'));
---
+++ src/funcoes.ts
@@ -0,0 +1,12 @@
+function multiplica(a: number ,b: number): number {
+    return a * b;
+}
+
+
+console.log(multiplica(5, 10))
+
+function saudacao(nome: string) : string {
+    return `Olá ${nome}`
+}
+
+console.log(saudacao('Bruno'))
