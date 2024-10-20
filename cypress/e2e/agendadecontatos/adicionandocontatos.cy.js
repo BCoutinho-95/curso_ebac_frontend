29ad6a54f9b4954dc5bf1e06031c56cd1d5de513
Bruno Coutinho <bcoutinhoti@gmail.com>
2024-10-20 16:04:24 -0300

    Update adicionandocontatos.cy.js

--- cypress/e2e/agendadecontatos/adicionandocontatos.cy.js
+++ cypress/e2e/agendadecontatos/adicionandocontatos.cy.js
@@ -3,34 +3,31 @@
 describe('Testes para agenda de contato', () => {
     beforeEach(() => {
         cy.visit('https://agenda-contatos-react.vercel.app/')
+        cy.clearAllLocalStorage()
     })
 
-    it('Deve adicionar 1 contato', () => {
+    
+    it('Deve adicionar um contato', () => {
         cy.get('input[type="text"]').type('bruno coutinho')
         cy.get('input[type="email"]').type('brunocoutinho@teste.com.br')
         cy.get('input[type="tel"]').type('32 12345678')
         cy.get('.adicionar').click()
-    })
-})
-
-describe('Testes para agenda de contato', () => {
-    beforeEach(() => {
-        cy.visit('https://agenda-contatos-react.vercel.app/')
+        cy.get('.sc-iAEyYk').should('contain','bruno coutinho')
     })
 
     it('Deve editar o contato cadastrado', () => {
-        cy.get('.edit').click()
-        cy.get('input[type="text"]').clear()
-        cy.get('input[type="email"]').clear()
-        cy.get('input[type="tel"]').clear()
-        cy.get('input[type="text"]').type('bruno marques coutinho')
-        cy.get('input[type="email"]').type('brunocoutinho@teste.com')
-        cy.get('input[type="tel"]').type('32 12345678')
+        cy.get('.sc-iAEyYk > :nth-child(2)')
+        cy.get(':nth-child(2) > .sc-gueYoa > .edit').click()
+        cy.get('input[type="text"]').clear().type('bruno coutinho')
+        cy.get('input[type="email"]').clear().type('brunocoutinho@teste.com')
+        cy.get('input[type="tel"]').clear().type('32 12345678')
         cy.get('.alterar').click()
+        cy.get('.sc-iAEyYk').should('contain','bruno coutinho')
     })
 
     it('Deve deletar o contato cadastrado', () => {
-        cy.get('.sc-beqWaB')
-        cy.get('.delete').click()
-    })
+        cy.get('.sc-iAEyYk > :nth-child(2)')
+        cy.get(':nth-child(2) > .sc-gueYoa > .delete').click()
+        cy.get('.sc-iAEyYk').should('not.contain', 'bruno coutinho')
+})
 })
